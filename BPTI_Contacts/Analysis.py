436a109b7012358ed9512454b3f2477fd93e6840
fnueske <feliks.nueske@fu-berlin.de>
2015-07-29 11:57:11 +0200

    Extended analysis of least-squares error for BPTI.

--- BPTI_Contacts/Analysis.py
+++ BPTI_Contacts/Analysis.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
 import sys
 fundamental_path = "/Users/fnueske/Documents/Uni/"
@@ -8,7 +7,7 @@ sys.path.append(fundamental_path + "TensorTrain/")
 import TensorTrain2.Util as TTU
 
 # Result directory:
-resdir = fundamental_path + "TTApplications/BPTI_Contacts/Results_eps99/"
+resdir = fundamental_path + "TTApplications/BPTI_Contacts/Results_eps995/"
 datadir = fundamental_path + "TTApplications/BPTI_Contacts/SwitchingFunction/"
 
 ''' Implied Timescales:'''
@@ -23,14 +22,14 @@ for m in range(nsweeps):
     plt.plot(xp2,ts[(m+0.5)*2*(d-2):(m+1)*2*(d-2)],label="Sweep%dB"%m)
 plt.legend(loc=4)
 plt.savefig(resdir+"Timescales.pdf")
-#plt.show()
+plt.show()
 
 ''' Least-Squares-Error:'''
 lsq = np.loadtxt(resdir + "LSQErrors.dat")
 plt.figure()
 plt.plot(lsq)
 plt.savefig(resdir+"LSQErrors.pdf")
-#plt.show()
+plt.show()
 
 ''' Histogram:'''
 # Physical time step:
@@ -44,36 +43,48 @@ ev_traj = [resdir + "Eigenfunction_%d.npy"%m for m in range(ntraj)]
 # Create a histogram:
 bins = 150
 m = np.array([1])
-#H = TTU.CreateEVHistogram(ev_traj,bins,filename,m=m)
+H = TTU.CreateEVHistogram(ev_traj,bins,filename,m=m)
 
 ''' Extract frames and show the contacts for these frames:'''
 # Define centers:
-c = np.array([[-0.2],[6.5]])
+c = np.array([[-6.5],[0.2]])
 # Define allowed distance to the centers:
-di = np.array([0.5,1])
+di = np.array([1,0.5])
 # Number of frames per traj and center:
 nframes = None
 # Write out trajs:
 indices = TTU.SaveEVFrames(dt,ev_traj,c,di,nframes=nframes)
- 
+  
 ''' Check agreement between least-squares error and contact.'''
- 
+  
 # Load the reduced basis trajectory:
 data = np.load(datadir + "SwitchingFunctionReduced.npy")
 #plt.figure()
 # Get the number of centers:
 nc = len(indices)
-npix = 40
-diff = np.zeros((3*npix,d))
+diff = np.zeros((2,d))
 for i in range(nc):
     ilist = indices[i][0][:,1]
     # Get the corresponding timesteps from the function trajectory:
     idata = data[ilist,:]
     # Compute the average value of each switching function:
-    diff[i*npix:(i+1)*npix,:] = np.mean(idata,axis=0)
-indlsq = np.where(lsq >= 0.01)[0]
-diff[2:npix:3*npix,indlsq] = 1.0
+    diff[i,:] = np.mean(idata,axis=0)
+    
+# Compute the difference between the averages in absolute value:
+diff = np.abs(diff[1,:]-diff[0,:])
+# Save them to file:
+np.savetxt(resdir + "DifferenceSwitchingFunction.dat",diff)
+# Create a normalized vector of the lsq errors:
+lsq2 = np.zeros(d)
+lsq2[1:d-1] = lsq/np.max(lsq)
+np.savetxt(resdir + "LSQErrorNormalized.dat",lsq2)
 
 plt.figure()
-plt.imshow(diff,cmap = cm.Greys_r)
+
+plt.subplot(2,1,1)
+plt.bar(np.arange(d), diff)
+
+plt.subplot(2,1,2)
+plt.semilogy(np.arange(d), lsq2,linestyle = 'None',marker='o',markersize=6)
+plt.savefig(resdir + "Comparison.pdf")
 plt.show() 
